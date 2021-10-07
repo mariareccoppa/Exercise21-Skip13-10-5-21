4fce97dbfc81b8b0d7e21795cf942a819462dc0a
Maria Reccoppa <83091797+mariareccoppa@users.noreply.github.com>
2021-10-07 18:04:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let floor = parseInt(prompt("What floor are you going to?"));
+if (floor < 13) console.log (floor);
+if (floor > 13) console.log(floor-1);
+else console.log("floor 13 does not exist!");
